333d76c5aceed8a2698753d12e0046c20704d450
icharlotte <zcharlotteyq@163.com>
2024-07-10 11:02:13 +0800

    update README

--- main.py
+++ main.py
@@ -65,7 +65,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--dataset",
         type=str,
-        help="[CAMRa2011, Mafengwo, MafengwoS, Weeplaces]",
+        help="[CAMRa2011, Mafengwo, MafengwoS, MovieLens, WeeplacesS]",
         default="Mafengwo",
     )
     parser.add_argument(
